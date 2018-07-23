f457ea0391a3cb948ecd26c558317e425aaad4a4
austinoboyle <14arob@queensu.ca>
2018-07-23 16:22:10 -0400

    Add course_num and letter params for quick CLI scrapes

--- scrape_solus/cli.py
+++ scrape_solus/cli.py
@@ -1,6 +1,8 @@
 import click
 import os
+import json
 from .scrape_jobs import scrape as parallel_scrape
+from .Scraper import Scraper
 
 
 @click.command()
@@ -10,6 +12,19 @@ from .scrape_jobs import scrape as parallel_scrape
               help='Output directory for data dump')
 @click.option('--deep', '-d', type=click.BOOL, default=True, help='Do you want Section Data?')
 @click.option('--headless', '-h', type=click.BOOL, default=True)
-def scrape(scrape_type, num_workers, output_dir, deep, headless):
-    parallel_scrape(output_folder=output_dir,
-                    scrape_type=scrape_type, processes=num_workers, headless=headless)
+@click.option('--letter', '-l', type=click.STRING)
+@click.option('--course_num', '-c', type=click.INT)
+def scrape(scrape_type, num_workers, output_dir, deep, headless, letter, course_num):
+    if letter:
+        scraper = Scraper(headless=headless)
+        if course_num:
+            results = scraper.scrape_specific_course(
+                letter=letter.upper(), course=course_num, deep=deep)
+        else:
+            results = scraper.scrape_page_by_letter(letter.upper(), deep=deep)
+        with open(os.path.join(output_dir, 'scrape_results.json'), 'w') as f:
+            json.dump(results, f)
+
+    else:
+        parallel_scrape(output_folder=output_dir,
+                        scrape_type=scrape_type, processes=num_workers, headless=headless)
