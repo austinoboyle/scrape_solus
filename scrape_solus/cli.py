b3e8ae2dd1416077de18d4cb713fb41964bdb0a3
austinoboyle <14arob@queensu.ca>
2018-07-24 12:08:09 -0400

    Update CLI for new scrape_specific_course functionality

--- scrape_solus/cli.py
+++ scrape_solus/cli.py
@@ -1,4 +1,6 @@
 import click
+from click import ClickException
+from selenium.common.exceptions import NoSuchElementException
 import os
 import json
 from .scrape_jobs import scrape as parallel_scrape
@@ -13,20 +15,32 @@ from .Scraper import Scraper
 @click.option('--deep', '-d', type=click.BOOL, default=True, help='Do you want Section Data?')
 @click.option('--headless', '-h', type=click.BOOL, default=True)
 @click.option('--letter', '-l', type=click.STRING)
-@click.option('--course_num', '-c', type=click.INT)
-def scrape(scrape_type, num_workers, output_dir, deep, headless, letter, course_num):
-    if letter:
+@click.option('--course_code', '-c', type=click.STRING)
+def scrape(scrape_type, num_workers, output_dir, deep, headless, letter, course_code):
+    if course_code:
+        scraper = Scraper(headless=headless)
+        try:
+            subject, code = tuple(course_code.strip().upper().split(' '))
+            results = scraper.scrape_specific_course(
+                subject=subject, code=code)
+            filename = os.path.join(output_dir, '{}.json'.format(course_code))
+            with open(filename, 'w') as f:
+                json.dump(results, f)
+        except NoSuchElementException:
+            raise ClickException(
+                "No Course Found with Code: {}".format(course_code))
+        except ValueError:
+            raise ClickException(
+                "Invalid Course Code - Proper format: 'APSC 123'")
+        except Exception as e:
+            print("UNKNOWN ERROR")
+            raise e
+    elif letter:
         scraper = Scraper(headless=headless)
         letter = letter.upper()
-        if course_num is not None:
-            results = scraper.scrape_specific_course(
-                letter=letter, course=course_num, deep=deep)
-            filename = os.path.join(
-                output_dir, '{}-{}.json'.format(letter, course_num))
-        else:
-            filename = os.path.join(
-                output_dir, '{}.json'.format(letter))
-            results = scraper.scrape_page_by_letter(letter, deep=deep)
+        filename = os.path.join(
+            output_dir, '{}.json'.format(letter))
+        results = scraper.scrape_page_by_letter(letter, deep=deep)
         with open(filename, 'w') as f:
             json.dump(results, f)
 
