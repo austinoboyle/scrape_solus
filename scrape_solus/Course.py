af4cb49f2945b08a0588bd14496e6057bf2d650b
austinoboyle <14arob@queensu.ca>
2018-07-25 00:03:08 -0400

    Fix issues bugs in loading empty Course Data

--- scrape_solus/Course.py
+++ scrape_solus/Course.py
@@ -5,43 +5,57 @@ class Course(object):
     def __init__(self, course, sections=False):
         self.course = course
 
+    def by_id(self, _id):
+        return self.course.find_element_by_id(_id)
+
+    def text_or_default(self, el, selector, default="UNKNOWN"):
+        try:
+            return el.find_element_by_css_selector(selector).text
+        except:
+            return default
+
+    def id_text(self, _id, with_default=True, default="UNKNOWN"):
+        try:
+            return self.by_id(_id).text
+        except NoSuchElementException:
+            print(
+                "COURSE: NO ELEMENT WITH ID: {}".format(_id))
+            return default
+
     def get_course_component_details(self, row):
         component = {}
-        component['name'] = row.find_element_by_css_selector(
-            'span[id^="DERIVED_CRSECAT_DESCR$"]').text
-        component['required'] = row.find_element_by_css_selector(
-            'span[id^="DERIVED_CRSECAT_DESCRSHORT$"]').text
+        component['name'] = self.text_or_default(
+            row, 'span[id^="DERIVED_CRSECAT_DESCR$"]')
+        component['required'] = self.text_or_default(
+            row, 'span[id^="DERIVED_CRSECAT_DESCRSHORT$"]'
+        )
         return component
 
     @property
     def details(self):
         details = {}
         try:
-            full_title = self.course.find_element_by_id(
-                'DERIVED_CRSECAT_DESCR200').text
+            full_title = self.id_text(
+                'DERIVED_CRSECAT_DESCR200', default='ZZZZ 999 - ERROR/UNKNOWN')
             details['code'] = full_title.split(' - ', 2)[0]
             details['title'] = full_title.split(' - ', 2)[1]
 
-            details['career'] = self.course.find_element_by_id(
-                'SSR_CRSE_OFF_VW_ACAD_CAREER$0').text
-            details['units'] = self.course.find_element_by_id(
-                'DERIVED_CRSECAT_UNITS_RANGE$0').text
-            details['grading_basis'] = self.course.find_element_by_id(
-                'SSR_CRSE_OFF_VW_GRADING_BASIS$0').text
-
+            details['career'] = self.id_text(
+                'SSR_CRSE_OFF_VW_ACAD_CAREER$0')
+            details['units'] = self.course.id_text(
+                'DERIVED_CRSECAT_UNITS_RANGE$0')
+            details['grading_basis'] = self.id_text(
+                'SSR_CRSE_OFF_VW_GRADING_BASIS$0')
+            details['campus'] = self.id_text(
+                'CAMPUS_TBL_DESCR$0')
+            details['academic_group'] = self.id_text(
+                'ACAD_GROUP_TBL_DESCR$0')
+            details['academic_organization'] = self.id_text(
+                'ACAD_ORG_TBL_DESCR$0')
             table = self.course.find_element_by_id('ACE_SSR_DUMMY_RECVW$0')
-            component_rows = table.find_elements_by_css_selector(
-                'tr')
-
+            component_rows = table.find_elements_by_tag_name('tr')
             details['course_components'] = list(
                 map(self.get_course_component_details, component_rows[1:]))
-
-            details['campus'] = self.course.find_element_by_id(
-                'CAMPUS_TBL_DESCR$0').text
-            details['academic_group'] = self.course.find_element_by_id(
-                'ACAD_GROUP_TBL_DESCR$0').text
-            details['academic_organization'] = self.course.find_element_by_id(
-                'ACAD_ORG_TBL_DESCR$0').text
         except NoSuchElementException:
             pass
         return details
